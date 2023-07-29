213732304d7c69a8006eb9a18c1fec1fcc39887b
Ian-ko <140904568+Ian-ko@users.noreply.github.com>
2023-07-29 19:54:34 +0200

    Create Parser.hs
    The parser

---
+++ Parser.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE ParallelListComp #-}
+
+text_match :: Eq a => [a] -> [a] -> Bool
+text_match pat input | (length pat) == (length input) = ls_and char_match
+                     | True = False
+                     where
+                     char_match = [x==y | x<-pat | y<-input]
+
+ls_and :: [Bool] -> Bool
+ls_and [] = True
+ls_and (x:xs) = x && (ls_and xs)
+
+word_extractor_core :: [Char] -> [Char] -> [[Char]]
+word_extractor_core word [] = [word]
+word_extractor_core word (x:xs) | (' '==x) || ('.' == x) = word:(word_extractor_core [] xs)
+                                | True = word_extractor_core (word++[x]) xs
+
+word_extractor :: [Char] -> [[Char]] 
+word_extractor input = (word_extractor_core [] input) -- >>= no_space
+
+ls_or :: [Bool] -> Bool
+ls_or [] = False
+ls_or (x:xs) = x || (ls_or xs)
+
+word_finder :: [Char] -> [Char] -> Bool
+word_finder pat input = ls_or matched_words
+ where
+ matched_words = fmap (text_match pat) (word_extractor input)
+
+--give in multiple keywords as string with spaces and give input
+multi_word_finder :: [Char] -> [Char] -> Bool
+multi_word_finder pat input = ls_and (fmap (\x -> word_finder x input) (word_extractor pat))
